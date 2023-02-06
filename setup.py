21c9e3d1f5e7d14031fe13964924d0fe140d00ef
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2023-02-06 23:56:35 +0900

    ファイル移動

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="mask_documents",
-    version="0.0.13",
+    version="0.0.14",
     description="私が整理したデータを公開しています。",
     author="Mask_coins",
     url="https://github.com/Mask-coins/mask_documents",
