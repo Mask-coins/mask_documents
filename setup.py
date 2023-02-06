d6d2fd09b81c013f2d4f4553926a83505fb7458b
Mask-coins <uton_yayuyo_ics@yahoo.co.jp>
2023-02-06 16:45:20 +0900

    SPARQL問合せを可能にする．

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="mask_documents",
-    version="0.0.1",
+    version="0.0.2",
     description="私が整理したデータを公開しています。",
     author="Mask_coins",
     url="https://github.com/Mask-coins/mask_documents",
